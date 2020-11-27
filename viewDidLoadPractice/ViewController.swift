1cd20df9a4f50643ad52fa054becc1b890227240
krauserhuang <crescentdence@gmail.com>
2020-11-27 12:58:05 +0800

    Initial Commit

---
+++ viewDidLoadPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewDidLoadPractice
+//
+//  Created by Tai Chin Huang on 2020/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
